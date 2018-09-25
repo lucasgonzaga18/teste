5e174c93725b9921074032614416cd9b103448dd
Gonzzaga <luccasgonzaga182@gmail.com>
2018-09-25 13:01:52 -0300

    versao de teste

---
+++ mini-projeto/ItemPedido.cs
@@ -0,0 +1,17 @@
+﻿
+namespace mini_projeto
+{
+    class ItemPedido
+    {
+        private int quantidade;
+        private double desconto;
+        private Produto p;
+        public ItemPedido(int quantidade, double desconto, Produto p)
+        {
+            this.quantidade = quantidade;
+            this.desconto = desconto;
+            this.p = p;
+        }
+ 
+    }
+}
---
+++ mini-projeto/Produto.cs
@@ -0,0 +1,45 @@
+﻿namespace mini_projeto
+{
+    class Produto
+    {
+        private int codigo;
+        private string descricao;
+        private double preco;
+
+        public Produto(int codigo, string descricao, double preco)
+        {
+            this.codigo = codigo;
+            this.descricao = descricao;
+            this.preco = preco;
+
+        }
+        public void setCodigo(int codigo)
+        {
+            this.codigo = codigo;
+        }
+
+        public void setPreco(double preco)
+        {
+            this.preco = preco;
+        }
+
+        public void setDescricao(string descricao)
+        {
+            this.descricao = descricao;
+        }
+
+        public int getCodigo()
+        {
+            return codigo;
+        }
+        public double getPreco()
+        {
+            return preco;
+        }
+        public string getDesc()
+        {
+            return descricao;
+        }
+
+    }
+}
